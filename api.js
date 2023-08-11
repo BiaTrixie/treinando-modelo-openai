649bc875fc1f7d43279dc71006de0016c404e45a
BiaTrixie <bmoura276@gmail.com>
2023-08-11 13:41:46 -0300

    v1
    Código com erro json no uploadFile.js

--- api.js
+++ api.js
@@ -1,7 +1,12 @@
-import { Configuration, OpenAIApi } from 'openai'
-const openaiApiKey = process.env.OPENAI_KEY
+import { Configuration, OpenAIApi } from 'openai';
+
+// Obtém a chave de API da variável de ambiente
+const openaiApiKey = process.env.OPENAI_KEY;
+
+// Configuração da instância da biblioteca openai
 const configuration = new Configuration({
   apiKey: openaiApiKey
-})
+});
 
-export const openai = new OpenAIApi(configuration)
+// Cria a instância da API OpenAI com a configuração fornecida
+export const openai = new OpenAIApi(configuration);
--- uploadFile.js
+++ uploadFile.js
@@ -5,9 +5,10 @@ async function upload() {
   try {
     const response = await openai.createFile(
       fs.createReadStream('./data_prepared.jsonl'),
-      "fine-tune"
+      'fine-tune'
     );
     console.log('File ID: ', response.data.id)
+    fs.writeFileSync('./fileId.js', `export const fileId = "${response.data.id}"`)
   } catch (err) {
     console.log('err: ', err)
   }
